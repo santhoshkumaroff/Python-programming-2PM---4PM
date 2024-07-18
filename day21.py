0016488455169e4b8a352c6c3170d47c6df96af5
santhoshkumar <ssanthoshkumar.official@gmail.com>
2024-07-18 15:58:59 +0530

    Changes

--- day21.py
+++ day21.py
@@ -1,3 +1,4 @@
+#perfect number
 def perfect(n):
     sum=0
     for i in range(1,n):
@@ -6,7 +7,19 @@ def perfect(n):
     return sum ==n
 # l=[i for i in range(1,50) if perfect(i)]
 # print(*l)
-
+#checkprime
+def checkprime(n):
+    l=[]
+    for i in range(1,n+1):
+        if n%i==0:
+            l+=[i]
+    return len(l)==2
+#amstrong
+def amstrong(n):
+    sum=0
+    for i in str(n):
+        sum+=int(i)**len(str(n))
+    return sum==n
 def hcf(n1,n2):
     hcf=0
     for i in range(1,min(n1,n2)+1):
@@ -19,12 +32,45 @@ def hcf(n1,n2):
 
 #normal method
 # def fact(n):
-    fact=1
-    for i in range(n,0,-1):
-        fact*=i
-    return fact
+    # fact=1
+    # for i in range(n,0,-1):
+    #     fact*=i
+    # return fact
+#factorial
 def fact(n):
-    if n==1:
+    if n==0:
         return 1
     return n*fact(n-1)
-print(fact(6))
+# print(fact(3))
+#strongnumber
+def strong(n):
+    sum=0
+    for i in str(n):
+        sum+=fact(int(i))
+    return n==sum
+# print(strong(10))
+
+# for i in range(1,500):
+#     if strong(i):
+#         print(i,end=" ")
+        
+# print(*[i for i in range(1,500) if strong(i)])
+
+def numberslogic():
+    option = int(input('1. check prime\n2. amstrong\n3. Factorial\n4.Strong\n5.Perfect\nEnter Your Option : '))
+    match option:
+        case 1:
+            print(checkprime(int(input("Enter number : "))))
+        case 2:
+            print(amstrong(int(input("Enter number : "))))
+        case 3:
+            print(fact(int(input("Enter number : "))))
+        case 4:
+            print(strong(int(input("Enter number : "))))
+        case 5:
+            print(perfect(int(input("Enter number : "))))
+        case _:
+            print("Invalid Option")
+            numberslogic()
+numberslogic()
+        
